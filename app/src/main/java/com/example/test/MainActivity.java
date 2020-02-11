94d942a07966848ace26120182a03b3915fe4f42
Icekraks <stjimmy149@live.com.au>
2020-02-11 18:34:03 +1100

    added printer code works with purge

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -1,76 +1,141 @@
 package com.example.test;
 
-import com.brother.ptouch.sdk.*;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
 
 import android.bluetooth.BluetoothAdapter;
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.print.PrintHelper;
+import com.brother.ptouch.sdk.*;
 
-
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
+import java.util.Scanner;
+import java.util.Set;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_ENABLE_BT = 55;
     BluetoothAdapter bluetooth = BluetoothAdapter.getDefaultAdapter();
-    //TODO: FIX NULL POINTER BLUETOOT
+    Printer myPrinter;
+
+    private static String PrinterName;
+    private static String MacAddress;
 
     testObject lol;
-    public MainActivity(){
-        this.lol = new testObject(null,null,null);
+
+    public MainActivity() {
+        this.lol = new testObject(null, null, null);
     }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (!bluetooth.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+
+        Set<BluetoothDevice> pairedDevices = bluetooth.getBondedDevices();
+
+        if (pairedDevices.size() > 0) {
+            // There are paired devices. Get the name and address of each paired device.
+            for (BluetoothDevice device : pairedDevices) {
+                String deviceName = device.getName();
+                String deviceHardwareAddress = device.getAddress(); // MAC address
+                if (deviceName.equals("QL-820NWB8014")) {
+                    PrinterName = "QL-820NWB8014";
+                    MacAddress = deviceHardwareAddress;
+                }
+                System.out.println(deviceName);
+                System.out.println(deviceHardwareAddress);
+            }
+        }
+
+        myPrinter = new Printer();
+        PrinterInfo info = new PrinterInfo();
+        info.macAddress = MacAddress;
+        info.labelNameIndex = LabelInfo.QL700.W29H90.ordinal();
+        info.printerModel = PrinterInfo.Model.QL_820NWB;
+        info.printMode = PrinterInfo.PrintMode.FIT_TO_PAPER;
+        info.halftone = PrinterInfo.Halftone.ERRORDIFFUSION;
+        myPrinter.setPrinterInfo(info);
+        myPrinter.setBluetooth(bluetooth);
+        System.out.println(myPrinter.getPrinterStatus());
+        System.out.println(myPrinter.getSerialNumber());
+
+
         Button button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               printerMethod("Hello World");
+                printLabel();
             }
         });
 
-
-
+        //COMMENTED CODE, MASS DISCOVery of bluetooth devices.
+//        else {
+//            bluetooth.startDiscovery();
+//            IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+//            BroadcastReceiver receiver = new BroadcastReceiver() {
+//                @Override
+//                public void onReceive(Context context, Intent intent) {
+//                    String action = intent.getAction();
+//                    if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+//                        // Discovery has found a device. Get the BluetoothDevice
+//                        // object and its info from the Intent.
+//                        BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+//                        String deviceName = device.getName();
+//                        String deviceHardwareAddress = device.getAddress(); // MAC address
+//                        System.out.println(deviceName);
+//                        System.out.println(deviceHardwareAddress);
+//                    }
+//
+//                }
+//            };
+//            registerReceiver(receiver, filter);
+//        }
     }
 
-    protected void getPrinter() {
+    protected void printLabel() {
         new Thread(new Runnable() {
             @Override
             public void run() {
-                Printer printer = new Printer();
-                List<BLEPrinter> printerList = printer.getBLEPrinters(BluetoothAdapter.getDefaultAdapter(), 30);
-                if(printerList.isEmpty()){
-                    System.out.println("failed to find printers");
-                } else {
-                    for (BLEPrinter p : printerList) {
-                        System.out.println("Local Name: " + p.localName);
-                        Log.d("TAG", "Local Name: " + p.localName);
+                if (myPrinter.startCommunication()) {
+//                    PrinterStatus result = myPrinter.printImage(bitmap);
+
+                    PrinterStatus result = myPrinter.printPdfFile();
+                    if (result.errorCode != PrinterInfo.ErrorCode.ERROR_NONE) {
+                        Log.d("TAG", "ERROR - " + result.errorCode);
                     }
+                    myPrinter.endCommunication();
                 }
             }
         }).start();
 
-    }
-    protected void printerMethod(String output){
-        System.out.println("lol1");
-        getPrinter();
-        System.out.println("after get printrer");
+
     }
 
-//    protected testObject getObject(){
-//        return lol;
-//    }
-//
-//    protected void setObjectValues(String name, Integer id, String qrCode){
-//        this.lol.Name = name;
-//        this.lol.Number = id;
-//        this.lol.QRCode = qrCode;
-//    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        // Don't forget to unregister the ACTION_FOUND receiver.
+
+    }
 
 }
 
